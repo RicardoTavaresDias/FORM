feac1bc62ac9ac96f9bb6648ee53ac5f0650605e
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-03-14 17:14:24 -0300

    versão 1.0.8

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -26,34 +26,34 @@ export function Form() {
   
   return (
     <>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <h3>Cadastro</h3>
-            <Input {...register('name')} type='text' errors={errors.name} placeholder='Nome' label='Nome' />
-            <Input {...register('email')} type='text' errors={errors.email} placeholder='email@email.com' label='Email' />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h3>Cadastro</h3>
+        <Input {...register('name')} type='text' errors={errors.name} placeholder='Nome' label='Nome' />
+        <Input {...register('email')} type='text' errors={errors.email} placeholder='email@email.com' label='Email' />
 
-          {/* <Street> */}
-            <CheckBox {...register('field')}  label='Campos Adicionais' />
+        {/* <Street> */}
+        <CheckBox {...register('field')}  label='Campos Adicionais' />
 
-          {field && (
-            <>
-              <div className="form-group" id='numbers'>
-                <div>
-                  <Input {...register('street.zipCode')} type='text' errors={errors.street?.zipCode} placeholder='00000-000' label='CEP' maxLength='9' id='cep'/>
-                </div>
-                <div>
+        {field && (
+          <>
+            <div className="form-group" id='numbers'>
+              <div>
+                <Input {...register('street.zipCode')} type='text' errors={errors.street?.zipCode} placeholder='00000-000' label='CEP' maxLength='9' id='cep'/>
+              </div>
+              <div>
                   <Input {...register('street.number')} type='text' errors={errors.street?.number} placeholder='Número' label='Número' id='number'/>
-                </div>
-               </div>
+              </div>
+            </div>
           
-               <Input {...register('street.address')} type='text' errors={errors.street?.address} placeholder='Endereço' label='Endereço' />
-               <Input {...register('street.complement')} type='text' errors={errors.street?.complement} placeholder='Complemento' label='Complemento' />
-            </>
-          )}
+            <Input {...register('street.address')} type='text' errors={errors.street?.address} placeholder='Endereço' label='Endereço' />
+            <Input {...register('street.complement')} type='text' errors={errors.street?.complement} placeholder='Complemento' label='Complemento' />
+          </>
+        )}
 
-          {/* </Street> */}
+        {/* </Street> */}
 
-          <Button disabled={isSubmitting} >{isSubmitting ? 'Loading...' : 'Enviar'}</Button>
-        </form>
+        <Button disabled={isSubmitting} >{isSubmitting ? 'Loading...' : 'Enviar'}</Button>
+      </form>
       <div id='screm'>{screm}</div>
     </>
   )
